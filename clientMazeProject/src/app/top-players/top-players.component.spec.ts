19deb887c54b3bbcaeefbed2f2bcdeaa00c49867
OxyWorgon <52650715+OxyWorgon@users.noreply.github.com>
2020-11-08 21:01:44 -0500

    Add files via upload

---
+++ clientMazeProject/src/app/top-players/top-players.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TopPlayersComponent } from './top-players.component';
+
+describe('TopPlayersComponent', () => {
+  let component: TopPlayersComponent;
+  let fixture: ComponentFixture<TopPlayersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TopPlayersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TopPlayersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
