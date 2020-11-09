7b518ecb4c3fa9ab29b2fdc316fd15788159a3d5
Jasmine Fenton <m_fenton61750@fanshaweonline.ca>
2020-11-08 21:40:47 -0500

    top players connection

--- clientMazeProject/src/app/app-routing.module.ts
+++ clientMazeProject/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AchievementsComponent} from './achievements/achievements.component';
+import {TopPlayersComponent} from './top-players/top-players.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'achievements', component: AchievementsComponent},
+  {path: 'top-players', component: TopPlayersComponent},
   {path: '', component: HomeComponent}
 ];
 
